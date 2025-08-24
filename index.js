611bab412b9270e0ad60e8c6e22c512440141b42
oscarstdlib <reparobypass@outlook.com>
2025-08-23 21:59:47 -0500

    co

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ app.use(cors({
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization']
 }));
+
+app.options("*", cors());
 // Middleware JWT
 function authenticateToken(req, res, next) {
   const token = req.headers['authorization']?.split(' ')[1];
